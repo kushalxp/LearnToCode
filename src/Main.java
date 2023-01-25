7f8403026389d2d10474bb31a88f53f16c695554
vijame <vj61092@gmail.com>
2023-01-25 19:38:13 +0900

    Added Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ import Vijay.Week1.MainVijay;
 public class Main {
 
     public static void main(String[] args) {
-        MainKushal kushal = new MainKushal();
-        MainGayatri gayatri = new MainGayatri();
+        //MainKushal kushal = new MainKushal();
+        //MainGayatri gayatri = new MainGayatri();
         MainVijay vijay = new MainVijay();
 
-        kushal.run();
-        gayatri.run();
+        //kushal.run();
+        //gayatri.run();
         vijay.run();
     }
 }
